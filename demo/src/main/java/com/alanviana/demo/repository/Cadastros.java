b200f82df0d7f29a968cb89c399b1e9c08a823e3
alanvianaa <alanviana.nascimento@gmail.com>
2019-07-30 10:09:32 -0300

    atualizando

--- demo/src/main/java/com/alanviana/demo/repository/Cadastros.java
+++
@@ -1,9 +0,0 @@
-
-package com.alanviana.demo.repository;
-
-import com.alanviana.demo.model.Cadastro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Cadastros extends JpaRepository<Cadastro, Long>{
-    
-}
